6d87d97543e5878e83baec501f4730447b55ac8a
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2023-10-26 13:28:27 +0200

    parsing models to views plan

--- editor_app/views.py
+++ editor_app/views.py
@@ -5,10 +5,12 @@ from django.contrib.auth.decorators import login_required
 
 
 @login_required
-def editor_panel_view(request):
-    return render(request, 'editor_panel.html')
+def editor_panel_view(request, editor_id):
+    editor_reports = editor_id  # TODO: get list of reports assigned to editor
+    return render(request, 'editor_panel.html', {'editor_reports': editor_reports})
 
 
 @login_required
 def master_editor_panel_view(request):
-    return render(request, 'master_editor_panel.html')
+    all_reports = []  # TODO: get list of all reports
+    return render(request, 'master_editor_panel.html', {'all_reports': all_reports})
--- user_app/views.py
+++ user_app/views.py
@@ -14,20 +14,26 @@ def search_view(request):
 
 @login_required
 def create_article_view(request):
-    return render(request, 'create_article.html')
+    article_form = set()  # TODO: parse article creation form
+    return render(request, 'create_article.html', {'article_form': article_form})
 
 
 @login_required
 def edit_article_view(request, article_id):
-
-    return render(request, 'edit_article.html')
+    article_form = []  # TODO: parse article creation form
+    # TODO: verify that user is owner of ther article
+    article = article_id  # TODO: get article from article_id
+    return render(request, 'edit_article.html', {'article': article, 'article_form': article_form})
 
 
 @login_required
-def create_report_view(request):
-    return render(request, 'create_report.html')
+def create_report_view(request, article_id):
+    report_form = []  # TODO: parse report creation form
+    return render(request, 'create_report.html', {'article_id': article_id, 'report_form': report_form})
 
 
 @login_required
 def user_panel_view(request):
+    user_articles = []  # TODO: parse user articles
+    user_reports = []  # TODO: parse user reports
     return render(request, 'user_panel.html')
