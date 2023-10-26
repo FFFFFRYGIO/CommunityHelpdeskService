ed313a86a3bc2d54f3c27cb7b15bfb00794be759
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2023-10-26 17:37:54 +0200

    Article, ArticleStep and Report models

--- editor_app/models.py
+++ editor_app/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from registration.models import User
+from user_app.models import Article
 
 # Create your models here.
 
 
-# TODO: report model
+class Report(models.Model):
+    # id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='report_author')
+    created_at = models.DateField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    editor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='report_editor')
+    additional_file = models.FileField()
--- user_app/models.py
+++ user_app/models.py
@@ -1,6 +1,23 @@
 from django.db import models
+from registration.models import User
 
 # Create your models here.
 
 
-# TODO: article model
+class Article(models.Model):
+    # id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField()
+    tags = models.JSONField(default=list)
+
+
+class ArticleStep(models.Model):
+    # id = models.IntegerField(primary_key=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    step_number = models.IntegerField()
+    title = models.CharField(max_length=255)
+    description1 = models.TextField()
+    file1 = models.FileField()
+    description2 = models.TextField()
+    file2 = models.FileField()
