dea721dfc907a9964fc90537b5219ff9dafa8940
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2023-10-29 21:49:26 +0100

    fix path names for editor_app

--- editor_app/urls.py
+++ editor_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("editor_panel/", views.editor_panel_view, name="home"),
-    path("master_editor_panel/", views.master_editor_panel_view, name="home"),
+    path("editor_panel/", views.editor_panel_view, name="editor_panel"),
+    path("master_editor_panel/", views.master_editor_panel_view, name="master_editor_panel"),
 ]
