79ecc81c80575010e419fde06f950ea1db17b726
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2023-10-30 19:11:54 +0100

    parse needed article it to edit article or create report

--- user_app/urls.py
+++ user_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("home/", views.home_view, name="home"),
     path("search/", views.search_view, name="search"),
     path("create_article/", views.create_article_view, name="create_article"),
-    path("edit_article/", views.edit_article_view, name="edit_article"),
-    path("create_report/", views.create_report_view, name="create_report"),
+    path("edit_article/<int:article_id>", views.edit_article_view, name="edit_article"),
+    path("create_report/<int:article_id>", views.create_report_view, name="create_report"),
     path("user_panel/", views.user_panel_view, name="user_panel"),
 ]
