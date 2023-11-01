eb736306477b4c7ce0b3065005ac1e5a6b9b19d5
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2023-11-01 14:44:34 +0100

    report list id parsing for editors and master editor

--- editor_app/views.py
+++ editor_app/views.py
@@ -6,12 +6,12 @@ from django.contrib.auth.decorators import login_required
 
 
 @login_required
-def editor_panel_view(request, editor_id):
-    editor_reports = editor_id  # TODO: get list of reports assigned to editor
+def editor_panel_view(request):
+    editor_reports = request.user.id  # TODO: get list of reports assigned to editor
     return render(request, 'editor_panel.html', {'editor_reports': editor_reports})
 
 
 @login_required
 def master_editor_panel_view(request):
-    all_reports = []  # TODO: get list of all reports
+    all_reports = request.user.id  # TODO: get list of all reports
     return render(request, 'master_editor_panel.html', {'all_reports': all_reports})
