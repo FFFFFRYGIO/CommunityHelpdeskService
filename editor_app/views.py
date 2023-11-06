ba68699574028f28c8efb9868eda1ce181dc0617
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2023-11-06 14:59:55 +0100

    editor and master editor panel views

--- editor_app/views.py
+++ editor_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Report
 
 
 # In your views.py
@@ -7,11 +8,19 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def editor_panel_view(request):
-    editor_reports = request.user.id  # TODO: get list of reports assigned to editor
-    return render(request, 'editor_panel.html', {'editor_reports': editor_reports})
+    """ editor panel with the assigned reports """
+    if request.user.groups.values_list('name', flat=True).filter(name='Editors'):
+        editor_reports = Report.objects.filter(editor=request.user)
+        return render(request, 'editor_panel.html', {'editor_reports': editor_reports})
+    else:
+        return redirect("home")
 
 
 @login_required
 def master_editor_panel_view(request):
-    all_reports = request.user.id  # TODO: get list of all reports
-    return render(request, 'master_editor_panel.html', {'all_reports': all_reports})
+    """ master editor panel with the all reports """
+    if request.user.groups.values_list('name', flat=True).filter(name='Editors'):
+        all_reports = Report.objects.all()
+        return render(request, 'master_editor_panel.html', {'all_reports': all_reports})
+    else:
+        return redirect("home")
