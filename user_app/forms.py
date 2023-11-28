97adf6cbebeda8a04a78b2c8240ded278bb6c2fb
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2023-11-28 23:16:48 +0100

    update create article template

--- user_app/forms.py
+++ user_app/forms.py
@@ -7,6 +7,11 @@ class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
         exclude = ['author', 'created_at', 'status']
+        widgets = {
+            'title': forms.TextInput(attrs={'required': True, 'class': 'form-control form-control-sm'}),
+            'tags': TagWidget(
+                attrs={'class': 'form-control form-control-sm'}),
+        }
 
 
 class StepForm(forms.ModelForm):
@@ -14,10 +19,19 @@ class StepForm(forms.ModelForm):
         model = Step
         exclude = ['article', 'ordinal_number']
         widgets = {
-            'title': forms.TextInput(attrs={'required': True}),
-            'description1': forms.Textarea(attrs={'rows': 1, 'cols': 40}),
-            'description2': forms.Textarea(attrs={'rows': 1, 'cols': 40}),
+            'title': forms.TextInput(
+                attrs={'class': 'form-control form-control-sm', 'required': True, 'placeholder': 'Title of the step'}),
+            'description1': forms.Textarea(attrs={'class': 'form-control form-control-sm', 'rows': 2, 'cols': 40,
+                                                  'placeholder': 'First description for the step'}),
+            'file1': forms.FileInput(
+                attrs={'class': 'form-control form-control-sm', 'type': 'file',
+                       'placeholder': 'First image for the step'}),
+            'description2': forms.Textarea(attrs={'class': 'form-control form-control-sm', 'rows': 2, 'cols': 40,
+                                                  'placeholder': 'Second description for the step'}),
+            'file2': forms.FileInput(attrs={'class': 'form-control form-control-sm', 'type': 'file',
+                                            'placeholder': 'Second image for the step'}),
         }
+        labels = {key: '' for key in ('title', 'description1', 'file1', 'description2', 'file2')}
 
 
 StepFormSetCreate = forms.modelformset_factory(Step, form=StepForm)
