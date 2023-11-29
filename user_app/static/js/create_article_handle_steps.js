d154ffa859d9d2c9fc52ba7b228e3e430184aae8
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2023-11-29 22:45:30 +0100

    move js script to separate file

---
+++ user_app/static/js/create_article_handle_steps.js
@@ -0,0 +1,27 @@
+let addStepButton = document.getElementById("add-step-button");
+let removeStepButton = document.getElementById("remove-step-button");
+let stepsFormset = document.getElementById("steps-formset");
+let stepForms = document.querySelectorAll(".step-form");
+let totalFormsInput = document.querySelector('input[name$="TOTAL_FORMS"]');
+
+addStepButton.addEventListener("click", function(e) {
+    if (e.target && e.target.classList.contains("add-step-button")) {
+        e.preventDefault();
+        let newStepForm = stepForms[0].cloneNode(true);
+        newStepForm.innerHTML = newStepForm.innerHTML.replace(/form-0/g, "form-" + totalFormsInput.value);
+        totalFormsInput.value = parseInt(totalFormsInput.value, 10) + 1;
+        newStepForm.innerHTML = newStepForm.innerHTML.replace(/Step 1/g, "Step " + totalFormsInput.value);
+        stepsFormset.appendChild(newStepForm);
+    }
+});
+removeStepButton.addEventListener("click", function(e) {
+    if (e.target && e.target.classList.contains("remove-step-button")) {
+        e.preventDefault();
+        if (totalFormsInput.value > 1) {
+            stepsFormset.removeChild(stepsFormset.lastChild);
+            totalFormsInput.value = parseInt(totalFormsInput.value, 10) - 1;
+        } else {
+            alert("You have to have at least 1 step");
+        }
+    }
+});
