65e8522fe31dfc70789c82f5e9216dfb89192af2
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2023-12-01 15:29:08 +0100

    code style change

--- user_app/forms.py
+++ user_app/forms.py
@@ -23,9 +23,8 @@ class StepForm(forms.ModelForm):
                 attrs={'class': 'form-control form-control-sm', 'required': True, 'placeholder': 'Title of the step'}),
             'description1': forms.Textarea(attrs={'class': 'form-control form-control-sm', 'rows': 2, 'cols': 40,
                                                   'placeholder': 'First description for the step'}),
-            'file1': forms.FileInput(
-                attrs={'class': 'form-control form-control-sm', 'type': 'file',
-                       'placeholder': 'First image for the step'}),
+            'file1': forms.FileInput(attrs={'class': 'form-control form-control-sm', 'type': 'file',
+                                            'placeholder': 'First image for the step'}),
             'description2': forms.Textarea(attrs={'class': 'form-control form-control-sm', 'rows': 2, 'cols': 40,
                                                   'placeholder': 'Second description for the step'}),
             'file2': forms.FileInput(attrs={'class': 'form-control form-control-sm', 'type': 'file',
