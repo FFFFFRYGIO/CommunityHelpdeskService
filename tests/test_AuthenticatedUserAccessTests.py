883c13eaed4b122b5af6b2343a6331fadb661f12
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2023-12-03 02:26:34 +0100

    use login view to log in authenticated user

--- tests/test_AuthenticatedUserAccessTests.py
+++ tests/test_AuthenticatedUserAccessTests.py
@@ -1,4 +1,9 @@
+from django.contrib.auth.models import User
+
 from tests.AccessTestBase import AccessTestsBase
+from tests.MainTestBase import USERS
+
+from django.urls import reverse
 
 
 # Create your tests here.
@@ -6,5 +11,11 @@ from tests.AccessTestBase import AccessTestsBase
 
 class AuthenticatedUserAccessTests(AccessTestsBase):
     def setUp(self):
-        super().setUp()
-        self.client.force_login(self.test_users['users'][0])
+        response = self.client.post(reverse('login'),
+                                    data={'username': USERS[0]['username'], 'password': USERS[0]['password']})
+        self.assertRedirects(response, reverse('home'))
+        self.user = User.objects.get(username=USERS[0]['username'])
+
+    def tearDown(self):
+        response = self.client.post(reverse('logout'))
+        self.assertRedirects(response, reverse('login'))
