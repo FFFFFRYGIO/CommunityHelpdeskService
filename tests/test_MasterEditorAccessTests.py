68363e97894a58fe94869271edeeed0cc5f6f18f
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2023-12-03 03:05:17 +0100

    change logging for MasterEditorAccessTests

--- tests/test_MasterEditorAccessTests.py
+++ tests/test_MasterEditorAccessTests.py
@@ -1,4 +1,9 @@
+from django.contrib.auth.models import User
+
 from tests.AccessTestBase import AccessTestsBase
+from tests.MainTestBase import USERS
+
+from django.urls import reverse
 
 
 # Create your tests here.
@@ -7,4 +12,15 @@ from tests.AccessTestBase import AccessTestsBase
 class MasterEditorAccessTests(AccessTestsBase):
     def setUp(self):
         super().setUp()
-        self.client.force_login(self.test_users['master_editors'][0])
+        response = self.client.post(reverse('login'),
+                                    data={'username': USERS[4]['username'], 'password': USERS[4]['password']})
+        self.assertRedirects(response, reverse('home'))
+        self.user = User.objects.get(username=self.test_users['master_editors'][0].username)
+
+    def tearDown(self):
+        super().tearDown()
+        response = self.client.post(reverse('logout'))
+        self.assertRedirects(response, reverse('login') + "?next=" + reverse('logout'))
+
+
+del AccessTestsBase
