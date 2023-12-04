ddc832bd8cbfd0f4a8c4e5d0e3f4e039303ab8d7
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2023-12-04 23:23:18 +0100

    add title to report model

--- editor_app/models.py
+++ editor_app/models.py
@@ -9,6 +9,7 @@ from user_app.models import Article
 class Report(models.Model):
     """ Report model structure """
     id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=150)
     description = models.TextField(max_length=512)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='report_author')
     created_at = models.DateTimeField()
