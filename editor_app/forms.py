8278fab300b70f61f34603451b7c943006ee15e2
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2023-12-04 16:37:27 +0100

    samove optionalization of additional file in report

--- editor_app/forms.py
+++ editor_app/forms.py
@@ -6,5 +6,3 @@ class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
         exclude = ['author', 'created_at', 'editor', 'status', 'article']
-
-    additional_file = forms.FileField(required=False)
