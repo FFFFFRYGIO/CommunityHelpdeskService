1a68e8269bd1c2ef8cd38cfbb39ee204cc4af48b
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2023-12-19 01:33:07 +0100

    add backend for filtering reports by master editor

--- editor_app/views.py
+++ editor_app/views.py
@@ -26,8 +26,27 @@ def editor_panel_view(request):
 def master_editor_panel_view(request):
     """ master editor panel with all reports """
     if request.user.groups.values_list('name', flat=True).filter(name='MasterEditors'):
-        all_reports = Report.objects.all()
-        return render(request, 'master_editor_panel.html', {'all_reports': all_reports})
+        filters_applied = {}
+        reports = Report.objects.all()
+        authors = User.objects.filter(report_author__in=reports).distinct()
+        statuses = Report.objects.values_list('status', flat=True).distinct()
+
+        if 'author_filter' in request.POST:
+            author_id = request.POST.get('author_filter')
+            print('author_id ', author_id)
+            if author_id:
+                reports = reports.filter(author_id=author_id)
+                filters_applied['author'] = User.objects.get(id=author_id).username
+
+        if 'status_filter' in request.POST:
+            status = request.POST.get('status_filter')
+            if status:
+                reports = reports.filter(status=status)
+                filters_applied['status'] = status
+
+        return render(request, 'master_editor_panel.html', {
+            'reports': reports, 'authors': authors, 'statuses': statuses, 'filters_applied': filters_applied})
+
     else:
         return redirect('home')
 
