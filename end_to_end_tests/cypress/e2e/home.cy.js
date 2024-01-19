32c54cfad06eb22d4d40f7e8bbb860aa77a35689
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2024-01-19 18:03:16 +0100

    create home end-to-end test

---
+++ end_to_end_tests/cypress/e2e/home.cy.js
@@ -0,0 +1,41 @@
+describe('template spec', () => {
+
+    beforeEach(() => {
+        cy.register_user('testuser', 'ph1shstix!');
+        cy.login_user('testuser', 'ph1shstix!');
+    });
+
+    it('Home Test', () => {
+
+        cy.visit('http://127.0.0.1:8000/user_app/home/');
+        cy.get('button').contains('Search articles').click();
+        cy.location().should((loc) => {
+            expect(loc.href).to.eq(
+                'http://127.0.0.1:8000/user_app/search/?'
+            );
+        });
+
+        cy.visit('http://127.0.0.1:8000/user_app/home/');
+        cy.get('button').contains('Create new article').click();
+        cy.location().should((loc) => {
+            expect(loc.href).to.eq(
+                'http://127.0.0.1:8000/user_app/create_article/?'
+            );
+        });
+
+        cy.visit('http://127.0.0.1:8000/user_app/home/');
+        cy.get('button').contains('Go to user panel').click();
+        cy.location().should((loc) => {
+            expect(loc.href).to.eq(
+                'http://127.0.0.1:8000/user_app/user_panel/?'
+            );
+        });
+
+    });
+
+    afterEach(() => {
+        cy.visit('http://127.0.0.1:8000/registration/logout');
+        cy.cleanup_user('testuser');
+    });
+
+});
