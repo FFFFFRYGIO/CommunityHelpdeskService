69d422c042af0b743869bf21ffc3d0bc3d58a5c3
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2024-01-21 17:51:20 +0100

    use fixtures in register end-to-end test

--- end_to_end_tests/cypress/e2e/register.cy.js
+++ end_to_end_tests/cypress/e2e/register.cy.js
@@ -2,20 +2,24 @@ describe('template spec', () => {
 
     it('Register Test', () => {
         cy.visit('http://127.0.0.1:8000/registration/register/');
-        cy.get('#id_username').type('testuser');
-        cy.get('#id_password1').type('ph1shstix!');
-        cy.get('#id_password2').type('ph1shstix!');
+        cy.fixture('user').then(user => {
+            cy.get('#id_username').type(user.username);
+            cy.get('#id_password1').type(user.password);
+            cy.get('#id_password2').type(user.password);
+        });
         cy.get('.registration_button').click();
         cy.location().should((loc) => {
             expect(loc.href).to.eq(
                 'http://127.0.0.1:8000/registration/login/'
             );
         });
-        cy.cleanup_user('testuser');
     });
 
     afterEach(() => {
-        cy.cleanup_user('testuser');
+        cy.visit('http://127.0.0.1:8000/registration/logout');
+        cy.fixture('user').then(user => {
+            cy.cleanup_user(user.username);
+        });
     });
 
 });
