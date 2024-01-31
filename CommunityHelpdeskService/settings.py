508531eab59d2d75deb38ce05a69a367a8427d7e
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2024-01-31 23:27:35 +0100

    use whitenoise for storing the files

--- CommunityHelpdeskService/settings.py
+++ CommunityHelpdeskService/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -145,3 +146,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 django_heroku.settings(locals())
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
